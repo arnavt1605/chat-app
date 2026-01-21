b9cf0bcd27022a526b08735360fd13e7dc12338a
Arnav <arnavtripathi1605@gmail.com>
2026-01-21 19:19:38 +0530

    update: add function to update profile picture

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
-import { generateToken } from "../lib/utils.js";
+import { generateToken } from "../lib/utils.js"
+import cloudinary from "../lib/cloudinary.js"
+
 
 
 export const signup = async (req, res) => {
@@ -81,8 +83,43 @@ export const login = async (req, res) => {
         console.log("Error in login controller:", error);
         res.status(500).json({ message: "Internal Server Error" });
     }
-}
+};
 
 export const logout = (req, res) => {
+    try {
+        res.cookie("jwt", "", {
+            maxAge: 0
+        });
 
+        res.status(200).json({ message: "Logged out successfully" });
+    }
+    catch (error) {
+        console.log("Error in logout controller:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+export const updateProfile = async (req, res) => {
+    try {
+        const { profilePic } = req.body;
+
+        const userId = req.user._id; // set in middleware
+
+        if (!profilePic) {
+            return res.status(400).json({ message: "Profile picture is required" });
+        }
+
+        // upload the picture
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+
+        // update in database
+        const updatedUser = await User.findByIdAndUpdate(userId, { profilePic: uploadResponse.secure_url }, { new: true })
+
+        res.status(200).json(updatedUser)
+    }
+    catch (error) {
+        console.log("Error in updating profile: ", error);
+        res.status(500).json({ message: "Internal Server Error" });
+
+    }
 }
