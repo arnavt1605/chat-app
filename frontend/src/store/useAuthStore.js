391a198660b6c9a8b10c3f9e0081e1f0a806fa5d
Arnav <arnavtripathi1605@gmail.com>
2026-01-28 17:18:57 +0530

    import toast

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 import { io } from "socket.io-client";
+import toast from "react-hot-toast";
 
 
 const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
